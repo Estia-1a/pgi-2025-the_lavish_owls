eed8269b631d0e333b3f078f91f59f5d6c951015
AndreaLEBLOA <190848891+AndreaLEBLOA@users.noreply.github.com>
2025-05-26 15:37:29 +0200

    Fix main.c

--- src/main.c
+++ src/main.c
@@ -32,11 +32,7 @@ int main(int argc, char **argv) {
     helloWorld();
   } else if (strncmp( configuration.command, "dimension", 3) == 0) {
     dimension(configuration.filenames[0]);
-  }
-  /*
-   * TO COMPLETE
-   */
-  if (strncmp(configuration.command, "first_pixel", 11) == 0) {
+  } else if (strncmp(configuration.command, "first_pixel", 11) == 0) {
     /* first_pixel() function is defined in feature.h and implemented in feature.c */
     first_pixel(configuration.filenames[0]);
   } else if (strncmp(configuration.command, "tenth_pixel", 11) == 0) {
